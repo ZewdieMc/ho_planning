2731bb48f627e09f065f5f24e20f0020bf4c1b40
Zewdie <zewdiehabtie26@gmail.com>
2024-04-20 15:18:49 +0200

    Implement RRT star

--- scripts/utils/global_planner.py
+++ scripts/utils/global_planner.py
@@ -1,25 +1,34 @@
+# Class to implement the RRT* algorithm
+import time
+
 import numpy as np
 from scipy.spatial import KDTree
 
-#* Class to implement the RRT algorithm
+
 class Planner:
-    def  __init__(self, state_validity_checker, max_iterations=10000, delta_q=2, p_goal=0.2, dominion=[-10, 10, -10, 10]):
-        # define constructor ...
+    def __init__(self, state_validity_checker, max_iterations=10000, delta_q=2, p_goal=0.2, dominion=[-10, 10, -10, 10], search_radius=5, time_limit= 5):
+        # Define constructor ...
         self.state_validity_checker = state_validity_checker
         self.max_iterations = max_iterations
         self.delta_q = delta_q
         self.p_goal = p_goal
         self.dominion = dominion
+        self.search_radius = search_radius
 
-        ##
+        # Initialize nodes, parent, and cost lists
         self.nodes = []
         self.parent = []
-    
+        self.cost = []
+        
+        # Time span for the planner
+        self.time_limit = time_limit
+
     def compute_path(self, q_start, q_goal):
-        # Implement RRT algorithm.
-        #* Use the state_validity_checker object to see if a position is valid or not.
+        time_start = time.time()
+        # Implement RRT* algorithm.
         self.nodes = [q_start]
         self.parent = [0]
+        self.cost = [0]
 
         for _ in range(self.max_iterations):
             q_rand = q_goal if np.random.rand() < self.p_goal else self.random_configuration()
@@ -27,19 +36,52 @@ class Planner:
             q_new = self.steer(q_near, q_rand)
 
             if self.state_validity_checker.is_valid(q_new) and self.state_validity_checker.check_path([q_near, q_new]):
+                print("..........")
+                neighbors = self.near(q_new)
+                min_cost = self.cost[near_idx] + np.linalg.norm(q_new - q_near)
+                min_idx = near_idx
+
+                # Find the node in the tree that can reach q_new with the lowest cost
+                for i in neighbors:
+                    new_cost = self.cost[i] + np.linalg.norm(q_new - self.nodes[i])
+                    if self.state_validity_checker.check_path([self.nodes[i], q_new]) and new_cost < min_cost:
+                        min_cost = new_cost 
+                        min_idx = i
+
                 self.nodes.append(q_new)
-                self.parent.append(near_idx)
-                
-                # avoid overshooting the goal #! needs to be checked
+                self.parent.append(min_idx)
+                self.cost.append(min_cost)
+
+                # Rewire the tree
+                for i in neighbors:
+                    if self.state_validity_checker.check_path([q_new, self.nodes[i]]) and self.cost[-1] + np.linalg.norm(self.nodes[i] - q_new) < self.cost[i]:
+                        self.parent[i] = len(self.nodes) - 1
+                        self.cost[i] = self.cost[-1] + np.linalg.norm(self.nodes[i] - q_new)
+
+                # Check if the goal can be reached
                 if np.linalg.norm(q_new - q_goal) < self.delta_q and self.state_validity_checker.check_path([q_new, q_goal]):
                     self.nodes.append(q_goal)
                     self.parent.append(len(self.nodes) - 2)
-                    raw_path = self.build_path(q_new)
+                    self.cost.append(min_cost + np.linalg.norm(q_goal - q_new))
+                    raw_path = self.build_path(q_goal)
                     smoothed_path = self.smooth_path(raw_path)
                     return smoothed_path
+            if time.time() - time_start > self.time_limit:
+                print("Time limit reached")
+                return [] #! No solution found within time limit, try with a higher time limit
         return []
-        
     
+
+    #* Find all nodes within a certain radius of q_new
+    def near(self, q_new):
+        radius = self.search_radius
+        nodes_in_radius = []
+        for i in range(len(self.nodes)):
+            if np.linalg.norm(np.array(self.nodes[i]) - np.array(q_new)) < radius:
+                nodes_in_radius.append(i)
+        return nodes_in_radius
+    
+
     def smooth_path(self, input_path):
         goal = len(input_path) - 1
         path = list(input_path)
